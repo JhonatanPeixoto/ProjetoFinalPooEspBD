793bedde859887b6052b7d98d665baaa35a00a9b
Marcielsousa <31634906+Marcielsousa@users.noreply.github.com>
2017-09-08 12:35:59 -0300

    Add files via upload

---
+++ ConsercionariaVeiculos/src/consercionariaveiculos/ModelException.java
@@ -0,0 +1,11 @@
+
+package br.ufg.inf.especializacao.databaseexample.model;
+
+public class ModelException extends RuntimeException{
+
+    public ModelException(String message) {
+        super(message);
+    }
+    
+    
+}
