3caded05b51b40c7db1ca4192a3a3a37e42fcb6f
jhonatan <jhonatan_peixoto@hotmail.com>
2017-09-11 21:59:05 -0300

    adição do Sonar

--- ConsercionariaVeiculos/src/consercionariaveiculos/ConsercionariaVeiculos.java
+++ ConsercionariaVeiculos/src/consercionariaveiculos/ConsercionariaVeiculos.java
@@ -16,6 +16,9 @@ public class ConsercionariaVeiculos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("OI");
+        
     }
     
 }
